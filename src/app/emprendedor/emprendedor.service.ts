3bd8c3b33a2de1cbf50ba363ee61984be5240fba
SantiagoJLopez <sj.lopezb1@uniandes.edu.co>
2025-11-19 08:43:14 -0500

    crear service

---
+++ src/app/emprendedor/emprendedor.service.ts
@@ -0,0 +1,17 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { EmprendedorDetail } from './emprendedor-detail';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class EmprendedorService {
+  private apiUrl: string = "https://raw.githubusercontent.com/k-garces/ISIS2603_202520_S4_P2_Practica/refs/heads/main/json/emprendedores.json";
+
+  constructor(private http: HttpClient) { }
+
+  getSponsors(): Observable<EmprendedorDetail[]> {
+    return this.http.get<EmprendedorDetail[]>(this.apiUrl);
+  }
+}
